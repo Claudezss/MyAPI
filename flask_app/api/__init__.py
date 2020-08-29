7df90b22906879fb74288a1c39ed8e79535b2f5f
Yan Zhang <yzhang@opusonesolutions.com>
2020-08-29 13:44:56 -0400

    fix ssl

--- flask_app/api/__init__.py
+++ flask_app/api/__init__.py
@@ -1,6 +1,17 @@
 from flask_restx import Api
 from .example import api as example_api_ns
+from flask import url_for
+import os
 
+ENV = os.environ.get("ENV", "")
+
+if ENV == "production":
+
+    @property
+    def specs_url(self):
+        return url_for(self.endpoint("specs"), _external=True, _scheme="https")
+
+    Api.specs_url = specs_url
 
 api = Api(title="Example API Docs", version="1.0", doc="/doc/", ordered=True)
 
